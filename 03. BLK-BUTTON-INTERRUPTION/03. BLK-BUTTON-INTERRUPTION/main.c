5d585e2a9b2ca60d80c6a65aa31ce58881a2a494
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-04-06 01:21:55 -0300

    06/04/2018
    Acender led quando um botão for pressionado. É usado interrupção nesse projeto.

---
+++ 03. BLK-BUTTON-INTERRUPTION/03. BLK-BUTTON-INTERRUPTION/main.c
@@ -0,0 +1,44 @@
+/*
+ * 03. BLK-BUTTON-INTERRUPTION.c
+ *
+ * Created: 06/04/2018 01:15:10
+ * Author : Antonio
+ *
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/* Replace with your application code */
+
+#define LED_ON		PORTB |= (1<<PORTB5)
+#define LED_OFF		PORTB &= ~(1<<PORTB5)
+#define LED_TOGGLE	PINB |= (1<<PINB5)
+#define  SWITCH_PRESSED !(PINB & (1<<PINB7))
+
+ISR(PCINT0_vect){
+	if(SWITCH_PRESSED)
+		LED_ON;
+	else
+		LED_OFF;
+	
+}
+
+int main(void){
+	
+	DDRB |= (1<<DDB5);
+	DDRB &= ~(1<<DDB7);
+	
+	
+	PCMSK0 |= (1<<PCINT7);
+	PCICR |= (1<<PCIE0);
+	
+	sei();
+	
+	while(1){
+		
+	}
+	
+}
+
+
